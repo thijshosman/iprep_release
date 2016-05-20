f58719610f1cd9b87678819a1b7044e092e44780
thijs <thosman@gatan.com>
2016-05-20 13:54:53 -0700

    -fixed minor bugs when trying to install

--- iPrep gtk scripts/iprep_general.s
+++ iPrep gtk scripts/iprep_general.s
@@ -370,10 +370,10 @@ class safetyFlags: object
 
 	object protected
 
-	void safetyFlags(object self)
-	{
-		persistentTag
-	}
+//	void safetyFlags(object self)
+//	{
+//		persistentTag
+//	}
 
 }
 
